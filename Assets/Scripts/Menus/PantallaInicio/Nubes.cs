cd3c015aaebe801f5c993ed275d1cb61f0decc2f
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-04-02 16:58:44 +0200

    Recomentado el script Nubes

--- Assets/Scripts/Menus/PantallaInicio/Nubes.cs
+++ Assets/Scripts/Menus/PantallaInicio/Nubes.cs
@@ -4,17 +4,21 @@
 
 public class Nubes : MonoBehaviour
 {
-     [SerializeField] float relacionVelocidad = 20;
+    //distancia que recorre las nubes, velocidad de movimiento
+    [SerializeField] float relacionVelocidad = 20;
     [SerializeField] float distanciaRecorrida = 25;
     float y = 0;
     float posIni;
+
     private void Start()
     {
+        //guardamos la posición en 'y'
         posIni = transform.position.y;
     }
 
     void Update()
     {
+        //movemos la nube oscilando en una distancia determinada
         y = Mathf.PingPong(Time.time * relacionVelocidad, distanciaRecorrida);
         transform.position = new Vector2(transform.position.x, posIni - y);
     }
