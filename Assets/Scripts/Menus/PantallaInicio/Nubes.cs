0120e2cd650f58557815227c6bdf8d28fc7d2c96
yojhansg <yojhansg@ucm.es>
2020-04-02 12:45:23 +0200

    Mejorado script Nubes del menú
    He reactivado el componente nubes y mejorado el script de las nubes con variables serializadas y ajustado para que funcione correctamente

--- Assets/Scripts/Menus/PantallaInicio/Nubes.cs
+++ Assets/Scripts/Menus/PantallaInicio/Nubes.cs
@@ -2,11 +2,18 @@
 
 public class Nubes : MonoBehaviour
 {
+    [SerializeField] float relacionVelocidad = 20;
+    [SerializeField] float distanciaRecorrida = 25;
     float y = 0;
+    float posIni;
+    private void Start()
+    {
+        posIni = transform.position.y;
+    }
 
     void Update()
     {
-        y = Mathf.PingPong(Time.time / 20, .1f);
-        transform.position = new Vector2(transform.position.x, y);
+        y = Mathf.PingPong(Time.time * relacionVelocidad, distanciaRecorrida);
+        transform.position = new Vector2(transform.position.x, posIni - y);
     }
 }
