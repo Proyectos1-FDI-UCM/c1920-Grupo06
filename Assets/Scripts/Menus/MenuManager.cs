faf11a1be0a8fbf86fa4dc9a825bcf8c02ec3edb
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-04-02 16:46:57 +0200

    Comentarios del resto de scripts. Desplazado el Material AntiBug a la carpeta Materiales

--- Assets/Scripts/Menus/MenuManager.cs
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -1,24 +1,29 @@
 ﻿using UnityEngine;
 
+//Control del comportamiento del menú principal
+
 public class MenuManager : MonoBehaviour
 {
+    //referencia a los distintos Lerps del menú
     [SerializeField] Lerp lerpIzquierdaScript;
     [SerializeField] Lerp lerpArribaScript;
     [SerializeField] Lerp lerpOrigenScript;
 
-    public void LerpIzquierda()
+    public void LerpIzquierda() //método para activar el Lerp a la izquierda
     {
         lerpOrigenScript.enabled = false;
         lerpArribaScript.enabled = false;
         lerpIzquierdaScript.enabled = true;
     }
-    public void LerpArriba()
+
+    public void LerpArriba() //método para activar el Lerp hacia arriba
     {
         lerpOrigenScript.enabled = false;
         lerpArribaScript.enabled = true;
         lerpIzquierdaScript.enabled = false;
     }
-    public void LerpOrigen()
+
+    public void LerpOrigen() //método para activar el Lerp hacia el origen
     {
         lerpOrigenScript.enabled = true;
         lerpArribaScript.enabled = false;
