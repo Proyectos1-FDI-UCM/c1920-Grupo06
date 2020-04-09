bc9fe09f24ba8ce00ca90cec227f2c04442c2007
yojhansg <yojhansg@ucm.es>
2020-04-09 18:29:43 +0200

    Añadido el script de control de estadísticas

---
+++ Assets/Scripts/Puntuacion/Estadísticas.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Estadísticas : MonoBehaviour
+{
+    private int numSaltos = 0;
+    private int numDash = 0;
+    private int numGanchos = 0;
+    private int numEnemigosDerrotados = 0;
+    private int numMuertes = 0;
+    public float tiempoJugado = 0;
+
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("saltos")) PlayerPrefs.SetInt("saltos", 0);
+        else PlayerPrefs.GetInt("saltos", numSaltos);
+
+        if (PlayerPrefs.HasKey("dash")) PlayerPrefs.SetInt("dash", 0);
+        else PlayerPrefs.GetInt("dash", numDash);
+
+        if (PlayerPrefs.HasKey("ganchos")) PlayerPrefs.SetInt("ganchos", 0);
+        else PlayerPrefs.GetInt("ganchos", numGanchos);
+
+        if (PlayerPrefs.HasKey("enemigos")) PlayerPrefs.SetInt("enemigos", 0);
+        else PlayerPrefs.GetInt("enemigos", numEnemigosDerrotados);
+
+        if (PlayerPrefs.HasKey("muertes")) PlayerPrefs.SetInt("muertes", 0);
+        else PlayerPrefs.GetInt("muertes", numMuertes);
+
+        if (PlayerPrefs.HasKey("tiempo")) PlayerPrefs.SetFloat("tiempo", 0);
+        else PlayerPrefs.GetFloat("tiempo", tiempoJugado);
+    }
+    public void Salto()
+    {
+        numSaltos++;
+    }
+    public void Dash()
+    {
+        numDash++;
+    }
+    public void Gancho()
+    {
+        numGanchos++;
+    }
+    public void Muerte()
+    {
+        numMuertes++;
+    }
+    public void Enemigo()
+    {
+        numEnemigosDerrotados++;
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("saltos", numSaltos);
+        PlayerPrefs.SetInt("dash", numDash);
+        PlayerPrefs.SetInt("ganchos", numGanchos);
+        PlayerPrefs.SetInt("enemigos", numEnemigosDerrotados);
+        PlayerPrefs.SetInt("muertes", numMuertes);
+        PlayerPrefs.SetFloat("tiempo", tiempoJugado);
+
+        PlayerPrefs.Save();
+    }
+}
