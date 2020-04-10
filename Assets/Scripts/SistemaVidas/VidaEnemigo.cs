908f9c328a21a49b4b1f7b7edf8048d51ba168b7
yojhansg <yojhansg@ucm.es>
2020-04-10 17:57:57 +0200

    Mejorado el comportamiento de las estadísticas
    Ahora hay menor uso de variables serializadas pues la referencia a las estadísticas se le da una sola vez al jugador y de ahí el resto de componentes la obtienen si tener que arrastrarle el script del editor a cada una de las entidades que necesiten dicha referencia

--- Assets/Scripts/SistemaVidas/VidaEnemigo.cs
+++ Assets/Scripts/SistemaVidas/VidaEnemigo.cs
@@ -4,13 +4,13 @@
 
 public class VidaEnemigo : MonoBehaviour
 {
-    [SerializeField] Estadísticas estadisticas = null;
     public void EliminarEnemigo(GameObject other)
     {
         //si el jugador esta haciendo dash/gancho sobre el enemigo, destruimos al enemigo
-        if (other != null && (other.GetComponent<Estados>().Estado() == estado.MovimientoGancho || other.GetComponent<Estados>().Estado() == estado.Dash))
+        Estados estados = other.GetComponent<Estados>();
+        if (other != null && (estados.Estado() == estado.MovimientoGancho || estados.Estado() == estado.Dash))
         {
-            estadisticas.Enemigo(); //Sumamos uno a enemigos derrotados
+            other.GetComponent<Jugador>().estadisticas.Enemigo(); //Sumamos uno a enemigos derrotados
             gameObject.SetActive(false);
             GameManager.instance.ContadorEnemigosElim();
         }
