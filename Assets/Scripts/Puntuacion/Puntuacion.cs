477f5377d26fa07a490fddde64b642620467bdc9
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-04-14 15:06:24 +0200

    Movida la escena Nivel1 fuera de la carpeta de pruebas

--- Assets/Scripts/Puntuacion/Puntuacion.cs
+++ Assets/Scripts/Puntuacion/Puntuacion.cs
@@ -3,34 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class Puntuacion : MonoBehaviour
 {
-    static int numMuertes1 = 9, numMuertes2 = 4;
-    static int enemigosElim1 = 5, enemigosElim2 = 3;
     static int puntuacion1 = 100, puntuacion2 = 1000;
 
-    public static void AñadirPuntuacion(int numMuertes, int enemigosElim, int puntuacion)
+    public static void AñadirPuntuacion(int puntuacion)
     {
-        if (SceneManager.GetActiveScene().buildIndex == 1)
-        {
-            numMuertes1 += numMuertes;
-            enemigosElim1 += enemigosElim;
-            puntuacion1 = puntuacion;
-        }
-        else
-        {
-            numMuertes2 += numMuertes;
-            enemigosElim2 += enemigosElim;
-            puntuacion2 = puntuacion;
-        }
-    }
-
-    public static int GetMuertes()
-    {
-        return numMuertes1 + numMuertes2;
-    }
-
-    public static int GetEnemEliminados()
-    {
-        return enemigosElim1 + enemigosElim2;
+        if (SceneManager.GetActiveScene().buildIndex == 1) puntuacion1 = puntuacion;
+        else puntuacion2 = puntuacion;
     }
 
     public static int GetPuntuacionNivel1()
@@ -45,8 +23,6 @@ public class Puntuacion : MonoBehaviour
 
     public static void ResetPuntuacion()
     {
-        numMuertes1 = 0; numMuertes2 = 0;
-        enemigosElim1 = 0; enemigosElim2 = 0;
         puntuacion1 = 0; puntuacion2 = 0;
     }
 }
