96fd38c3f4596fa9452425843bb5d127bb72fafe
yojhansg <yojhansg@ucm.es>
2020-04-23 11:01:01 +0200

    Finalizada la funcionalidad de la escena del menu

---
+++ Assets/Scripts/Puntuacion/PantallaEstadisticas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PantallaEstadisticas : MonoBehaviour
+{
+    Text texto = null;
+    void Start()
+    {
+        texto = GetComponent<Text>();
+
+        int numSaltos = 0, 
+            numDash = 0, 
+            numGanchos = 0, 
+            numEnemigosDerrotados = 0,
+            numMuertes = 0;
+        PlayerPrefs.GetInt("saltos", numSaltos);
+        PlayerPrefs.GetInt("dash", numDash);
+        PlayerPrefs.GetInt("ganchos", numGanchos);
+        PlayerPrefs.GetInt("enemigos", numEnemigosDerrotados);
+        PlayerPrefs.GetInt("muertes", numMuertes);
+
+
+        texto.text = "Número de saltos: " + numSaltos + "\n" +
+                     "Número de dashes: " + numDash + "\n" +
+                     "Número de ganchos: " + numGanchos + "\n" +
+                     "Número de enemigos derrotados: " + numEnemigosDerrotados + "\n" +
+                     "Número de muertes: " + numMuertes + "\n";
+    }
+}
