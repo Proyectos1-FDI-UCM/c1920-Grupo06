ef12503535601f8abc51efeec13b0adf4c0a7eb0
yojhansg <yojhansg@ucm.es>
2020-04-23 10:31:37 +0200

    Añadida una escena de prueba para puntuaciones

---
+++ Assets/Scripts/Puntuacion/PantallaPuntuaciones.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PantallaPuntuaciones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
