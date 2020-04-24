913a0778997c4ba2f8acf6c047bb609fa36a4053
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-04-24 13:03:27 +0200

    Añadido que el sprite del enganche se cambie dinamicamente

---
+++ Assets/Scripts/Obstaculos/SpritePlataformaGancho.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SpritePlataformaGancho : MonoBehaviour
+{
+    [SerializeField] Sprite enganche = null, plataformaGanchoIzq = null, plataformaGanchoDer= null;
+    [SerializeField] bool platGanchoIzq = false;
+
+    void Awake()
+    {
+        SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        if (transform.localScale.y == 1) spriteRenderer.sprite = enganche;
+        else
+        {
+            transform.localScale = new Vector2(transform.localScale.x * 1.5f, transform.localScale.y);
+            BoxCollider2D box = gameObject.GetComponent<BoxCollider2D>();
+            box.size = new Vector2(box.size.x * 0.5f, box.size.y);
+            if (platGanchoIzq) spriteRenderer.sprite = plataformaGanchoIzq;
+            else spriteRenderer.sprite = plataformaGanchoDer;
+        }
+    }
+}
