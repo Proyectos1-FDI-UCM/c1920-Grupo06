e2eda86c54c100a15f53d84d631c07a6322b4195
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-04-26 21:09:32 +0200

    Cambios en el menúManager y Lerp para que exista un movimiento a la izquierda. Añadido el texto de estadísticas al menú principal

--- Assets/Scripts/Menus/MenuManager.cs
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -6,37 +6,51 @@ public class MenuManager : MonoBehaviour
     [SerializeField] Lerp lerpDerecha;
     [SerializeField] Lerp lerpOrigen;
     [SerializeField] Lerp lerpArriba;
+    [SerializeField] Lerp lerpIzquierda;
 
     public void LerpDerecha()
     {
         //Si no se esta produciendo ningún movimiento Lerp, comienza el Lerp
-        if (lerpArriba.enabled == false && lerpOrigen.enabled == false && lerpDerecha.enabled == false)
+        if (lerpArriba.enabled == false && lerpIzquierda.enabled == false && lerpOrigen.enabled == false && lerpDerecha.enabled == false)
         {
             //Se desactivan los demás Lerp
             lerpDerecha.enabled = true;
             lerpOrigen.enabled = false;
             lerpArriba.enabled = false;
+            lerpIzquierda.enabled = false;
         }
     }
 
     public void LerpOrigen()
     {
-        if (lerpArriba.enabled == false && lerpOrigen.enabled == false && lerpDerecha.enabled == false)
+        if (lerpArriba.enabled == false && lerpIzquierda.enabled == false && lerpOrigen.enabled == false && lerpDerecha.enabled == false)
         {
             lerpDerecha.enabled = false;
             lerpOrigen.enabled = true;
             lerpArriba.enabled = false;
+            lerpIzquierda.enabled = false;
         }
     }
 
     public void LerpArriba()
     {
-        if (lerpArriba.enabled == false && lerpOrigen.enabled == false && lerpDerecha.enabled == false)
+        if (lerpArriba.enabled == false && lerpIzquierda.enabled == false && lerpOrigen.enabled == false && lerpDerecha.enabled == false)
         {
             lerpDerecha.enabled = false;
             lerpOrigen.enabled = false;
             lerpArriba.enabled = true;
+            lerpIzquierda.enabled = false;
         }
     }
-
+    
+    public void LerpIzquierda()
+    {
+        if (lerpArriba.enabled == false && lerpIzquierda.enabled == false && lerpOrigen.enabled == false && lerpDerecha.enabled == false)
+        {
+            lerpDerecha.enabled = false;
+            lerpOrigen.enabled = false;
+            lerpArriba.enabled = false;
+            lerpIzquierda.enabled = true;
+        }
+    }
 }
