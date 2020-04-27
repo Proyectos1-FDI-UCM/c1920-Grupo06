ef41592c0c6187dc33843364f782d7613bd7bf0c
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-04-27 17:14:06 +0200

    Añadido musica de fondo en el juego. Creado un AudioManager básico para que la musica no se corte. Arreglados sliders de los ajustes de la pantalla de inicio

---
+++ Assets/Scripts/Sonido/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource music = null;
+    static public AudioManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+
+        music.playOnAwake = true;
+        music.loop = true;
+        music.Play();
+    }
+
+    public void Pause()
+    {
+        music.Pause();
+    }
+
+    public void UnPause()
+    {
+        music.UnPause();
+    }
+}
