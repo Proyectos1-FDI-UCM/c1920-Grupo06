032c9f3c2fc3842dd8fe69db851f3fff49af2826
Pablo Fernandez Alvarez <pablfe20@ucm.es>
2020-04-29 21:04:11 +0200

    Implementada animación para la plataforma de tiempo

--- Assets/Scripts/Obstaculos/PlataformaTiempo.cs
+++ Assets/Scripts/Obstaculos/PlataformaTiempo.cs
@@ -5,7 +5,15 @@
 public class PlataformaTiempo : MonoBehaviour
 {
     [SerializeField] float TiempoDesaparicion = 0; //tiempo de desaparición
+    Animator romper = null;
+    bool tieneAnimator;
+    private void Start()
+    {
+        tieneAnimator = (GetComponent<Animator>() != null);
 
+        //guardamos el componente animator para la animación de la plataforma
+        if (tieneAnimator) romper = GetComponent<Animator>();
+    }
     void OnCollisionEnter2D(Collision2D collision) 
     {
         //guardamos los "bounds" y el componente de la colisión 
@@ -15,11 +23,20 @@ public class PlataformaTiempo : MonoBehaviour
         //si la colisión se produce sobre la plataforma y es el jugador
         if (estados != null && estados.gameObject.transform.position.y > transform.position.y + borde.extents.y)
         {
+            if(tieneAnimator) romper.SetBool("Contacto", true); //comienza la animación
             Invoke("Desactivar", TiempoDesaparicion); //hacemos que desaparezca tras cierto tiempo
         }
     }
     void Desactivar() //método para que desaparezca
     {
+        if (tieneAnimator)
+        {
+            //se cambia el estado de animación a "ResetPlatTiempo" que espera al booleano "Contacto"
+            romper.SetBool("Reinicio", true);
+            //se cambia el valor para que no vuelva a reproducirse la animación hasta que el jugador no vuelva a entrar en contacto con la plataforma
+            romper.SetBool("Contacto", false);
+        }
+
         gameObject.SetActive(false);
     }
 }
