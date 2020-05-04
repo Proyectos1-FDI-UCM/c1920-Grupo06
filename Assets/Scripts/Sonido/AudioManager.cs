f469d9e1674fcb5553eedf95c476d5d7b7d87b58
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-05-04 16:22:31 +0200

    Añadido sonido cada vez que decrece el cronometro en 1. Añadido aceleracion de la musica con respecto al tiempo. Cambiado parametros de cambio de color del cronometro (aumentado)

--- Assets/Scripts/Sonido/AudioManager.cs
+++ Assets/Scripts/Sonido/AudioManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
@@ -29,5 +27,23 @@ public class AudioManager : MonoBehaviour
     public void UnPause()
     {
         music.UnPause();
+    }
+
+    public void Acelerar(int contador)
+    {
+        //Aumentamos el pitch acorde al tiempo del cronometro
+        if (contador < 50)
+        {
+            if (contador <= 20)
+            {
+                if (contador <= 5) music.pitch = 1.3f;
+                else music.pitch = 1.2f;
+            }
+            else music.pitch = 1.1f;
+        }
+        else music.pitch = 1.0f;
+
+        //restablecemos el pitch, para que asi solo acelere el tempo (sin esto, se oiría más rapido pero mas agudo)
+        music.outputAudioMixerGroup.audioMixer.SetFloat("pitchMusica_", 1 / music.pitch);
     }
 }
