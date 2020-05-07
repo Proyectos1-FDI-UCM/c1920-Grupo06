740d77a6e2fd372e293f55219695344cea1fa824
iquintasALT <iquintas@ucm.es>
2020-05-08 01:38:29 +0200

    Creado el prefab ParticulasMuerteJugador. Integrado el funcionamiento de AgitarCamara y EfectJugadorDañado en los niveles jugables

--- Assets/EfectoJugadorDañado.cs
+++ Assets/EfectoJugadorDañado.cs
@@ -6,8 +6,9 @@ public class EfectoJugadorDañado : MonoBehaviour
 {
     [SerializeField] Color ColorA = Color.red;
     [SerializeField] Color ColorB = Color.green;
-    [SerializeField] Color ColorIni;
+    Color ColorIni;
     [SerializeField] float speed = 1.0f;
+    [SerializeField] GameObject PrefabParticulasMuerteJugador;
     float tiempoInvulnerable = 3f;
 
     SpriteRenderer spriteRenderer;
@@ -17,6 +18,17 @@ public class EfectoJugadorDañado : MonoBehaviour
         enabled = false;
         spriteRenderer = GetComponent<SpriteRenderer>();
         ColorIni = spriteRenderer.color;
+    }
+
+    private void OnEnable()
+    {
+        if (GameManager.instance.getVidas() <= 0)
+        {
+            GameObject particulas = Instantiate(PrefabParticulasMuerteJugador, transform.position, Quaternion.Euler(0f, 0f, 0f));
+            ParticleSystem particleSystem = particulas.GetComponent<ParticleSystem>();
+            particleSystem.Play();
+            enabled = false;
+        }
     }
 
     void Update()
