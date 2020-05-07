cebb3d9c4d9e52ae8261014411128e33101fad8a
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-05-07 20:12:11 +0200

    Cambios en los tamaños de la cámara, para que la interfaz se adapte. Continuar entre secciones del tutorial hecho, falta sript de secuencias de texto y avance, y poder salir del tutorial per se

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] Transform jugador = null, seccion2 = null, seccion3 = null, seccion4 = null;
+    bool seccion2Esta = false, seccion3Esta = false;
+
+    public void Continuar()
+    {
+        AsignacionTransform();
+        Camera.main.transform.position = new Vector3(jugador.position.x, jugador.position.y + 4, Camera.main.transform.position.z);
+    }
+
+    private void AsignacionTransform()
+    {
+        if (seccion2Esta)
+        {
+            if (seccion3Esta) jugador.transform.position = seccion4.transform.position;
+            else
+            {
+                jugador.transform.position = seccion3.transform.position;
+                seccion3Esta = true;
+            }
+        }
+        else
+        {
+            jugador.transform.position = seccion2.transform.position;
+            seccion2Esta = true;
+        }
+    }
+}
