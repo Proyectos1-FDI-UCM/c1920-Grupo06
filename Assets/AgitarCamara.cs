cf0b78479b8d3ba213822d2e272928e559db91e3
iquintasALT <iquintas@ucm.es>
2020-05-08 02:05:36 +0200

    Comentados los scripts AgitarCamara y EfectoJugadorDañado, además de algunas aclaraciones puntuales en VidaJugador. Modificado el nivel2 para el correcto funcionamiento de estos scripts.

--- Assets/AgitarCamara.cs
+++ Assets/AgitarCamara.cs
@@ -1,19 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+//Script que agita la cámara cuando el jugador recibe daño
 
 public class AgitarCamara : MonoBehaviour
 {
 
-	Vector3 PosIni;
+	Vector3 PosIni;  //posición inicial de la cámara
 	[SerializeField] float distancia = 0.05f, tiempoHastaDetenerse = 0.5f;
 	Camera Camara;
 
+	//empieza desactivado
 	private void Awake()
 	{
 		enabled = false;
 	}
 
+	//al activarse, la cámara se agitará durante "tiempoHastaDetenerse" segundos
 	private void OnEnable()
 	{
 		Camara = Camera.main;
@@ -22,23 +24,25 @@ public class AgitarCamara : MonoBehaviour
 		Invoke("PararAgitarCamara", tiempoHastaDetenerse);
 	}
 
+	//se cálcula una posición aleatoria en las x
 	void ComienzaAgitarCamara()
 	{
-		float cameraShakingOffsetX = Random.value * distancia * 2 - distancia;
-		Vector3 cameraIntermadiatePosition = Camara.transform.position;
-		cameraIntermadiatePosition.x += cameraShakingOffsetX;
-		Camara.transform.position = cameraIntermadiatePosition;
+		float limiteEnX = Random.value * distancia * 2 - distancia;
+		Camara.transform.position = new Vector3 (Camara.transform.position.x + limiteEnX, Camara.transform.position.y, Camara.transform.position.z);
 	}
 
+	//se termina el movimiento y se desactiva el script
 	void PararAgitarCamara()
 	{
 		CancelInvoke("ComienzaAgitarCamara");
 		enabled = false;
 	}
 
+	//al desactivarse
 	private void OnDisable()
 	{
-		if(Camara != null) Camara.transform.position = PosIni;
+		if(Camara != null) Camara.transform.position = PosIni; //se devuelve la cámara a su posición inicial
+		//una vez terminado el efecto visual de daño, si el jugador tiene sus vidas a 0, se muere
 		if (GameManager.instance != null && GameManager.instance.getVidas() <= 0) GameManager.instance.Muerte();
 	}
 }
