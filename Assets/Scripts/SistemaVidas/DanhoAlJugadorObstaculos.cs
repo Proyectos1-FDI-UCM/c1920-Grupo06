c23e1b8d7c8136392696700405389b79e046fc1e
ivasan07 <ivasan07@ucm.es>
2020-05-15 14:30:38 +0200

    Separado el knockback en otro script

--- Assets/Scripts/SistemaVidas/DanhoAlJugadorObstaculos.cs
+++ Assets/Scripts/SistemaVidas/DanhoAlJugadorObstaculos.cs
@@ -7,12 +7,12 @@ public class DanhoAlJugadorObstaculos : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other)
     {
         VidaJugador vida = other.gameObject.GetComponent<VidaJugador>(); //comprobamos si detecta a una entidad con vida (jugador)
-        Salto salto = other.gameObject.GetComponent<Salto>(); //comprobamos si detecta a una entidad con vida (jugador)
+        KnockBack kb = other.gameObject.GetComponent<KnockBack>(); //comprobamos si detecta a una entidad con vida (jugador)
 
         if (vida != null) //si detecta al jugador
         {
             vida.EliminaVidaObstaculos(); //llamamos al metodo de quitar vida del jugador    
-            salto.Knockback(transform.position);
+            kb.Knockback(transform.position);
         }
     }
 }
