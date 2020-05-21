f59fc5fb71fc9a4927f9f5428d6f0d33a3a21659
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-05-21 20:39:14 +0200

    Añadido Scroll a los creditos. Corregido error de los sonidos del Tutrial, más algunos retoques estéticos en este

---
+++ Assets/Scripts/Camara/Scroll.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Script del scroll de créditos
+
+public class Scroll : MonoBehaviour
+{
+    [SerializeField] Transform puntoAIr = null;
+    [SerializeField] float velocidad = 2.5f;
+    bool yaInvocado = false;
+
+    void Update()
+    {
+        if (transform.position.y > puntoAIr.position.y)
+            transform.position = Vector3.MoveTowards(transform.position, puntoAIr.position, velocidad * Time.deltaTime);
+        else if (!yaInvocado) Invoke("PararCinematico", 3f);
+    }
+
+    private void PararCinematico()
+    {
+        yaInvocado = true;
+        if (SceneManager.GetActiveScene().name == "Creditos") GameManager.instance.ChangeScene(0);
+    }
+}
