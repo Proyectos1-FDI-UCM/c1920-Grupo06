e08ddc18d60c4291ad49285b1933e2f4f319ee0e
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-05-21 16:07:41 +0200

    Añadida pantalla de Creditos (vacía)

---
+++ Assets/Scripts/Creditos.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+//Script para salir de los créditos
+
+public class Creditos : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKey) GameManager.instance.ChangeScene(0);
+    }
+}
