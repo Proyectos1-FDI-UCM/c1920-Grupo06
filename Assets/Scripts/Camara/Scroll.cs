cea679283d51f328c8a41aba4c7564148818e228
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-05-21 22:00:42 +0200

    Añadido LevelManager (Activa Nivel 2 en el Menu). Recolocado script Creditos

--- Assets/Scripts/Camara/Scroll.cs
+++ Assets/Scripts/Camara/Scroll.cs
@@ -11,11 +11,13 @@ public class Scroll : MonoBehaviour
 
     void Update()
     {
+        //se mueve a un punto
         if (transform.position.y > puntoAIr.position.y)
             transform.position = Vector3.MoveTowards(transform.position, puntoAIr.position, velocidad * Time.deltaTime);
         else if (!yaInvocado) Invoke("PararCinematico", 3f);
     }
 
+    //metodo para parar durante un periodo de tiempo la camara
     private void PararCinematico()
     {
         yaInvocado = true;
---
+++ Assets/Scripts/Menus/ActivarNivel2Menu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ActivarNivel2Menu : MonoBehaviour
+{
+    void Awake()
+    {
+        if (!LevelManager.instance.Nivel2()) gameObject.SetActive(false);
+        else gameObject.SetActive(true);
+    }
+}
