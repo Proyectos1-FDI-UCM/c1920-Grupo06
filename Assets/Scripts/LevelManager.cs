3ebba6027aeeeb94a6fe55d5044096734902c87e
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-05-21 22:00:42 +0200

    Añadido LevelManager (Activa Nivel 2 en el Menu). Recolocado script Creditos

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+//Script que se encarga de activar el nivel 2 en Menu (booleano)
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance = null;
+    bool nivel2 = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        Debug.Log(nivel2);
+    }
+
+    public void ActivarNivel2()
+    {
+        nivel2 = true;
+    }
+
+    public bool Nivel2()
+    {
+        return nivel2;
+    }
+}
