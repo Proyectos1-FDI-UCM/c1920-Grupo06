5499f5375f4ab331c44f45a7fb97d066b205ef83
ivasan07 <ivasan07@ucm.es>
2020-05-21 22:55:09 +0200

    Arreglada plataforma rompible V2
    Espero que esta vez no haya ningún fallo, he estado probando. La última vez se me olvidó ignorar el resto de plataformas y por eso daba fallos.

--- Assets/Scripts/Obstaculos/PlataformaAgrietada.cs
+++ Assets/Scripts/Obstaculos/PlataformaAgrietada.cs
@@ -4,35 +4,28 @@
 
 public class PlataformaAgrietada : MonoBehaviour
 {
-    BoxCollider2D col = null;
-    private void Start()
-    {
-        col = transform.parent.GetComponent<BoxCollider2D>();
-    }
 
+    bool ignora = false;
     void OnTriggerEnter2D(Collider2D collision) //al entrar en el "trigger" secundario (mayor que la plataforma)
     {
-        Estados estadoAct = collision.gameObject.GetComponent<Estados>(); //guardamos el estado
+        ignora = false;
+        Estados estadoAct = collision.gameObject.GetComponent<Estados>();
+        Dash dash = collision.gameObject.GetComponent<Dash>();
 
-        col.enabled = true;
-
-        //si ha entrado el jugador, y está usando el dash o el gancho
         if (estadoAct != null && (estadoAct.Estado() == estado.MovimientoGancho || estadoAct.Estado() == estado.Dash))
-        {
-            //destruimos la plataforma
             transform.parent.gameObject.SetActive(false);
-        }
     }
 
 
-    void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        Estados estadoJugador = collision.GetComponent<Estados>();
-        col.enabled = true;
+        Estados estadoAct = collision.gameObject.GetComponent<Estados>();
+        Dash dash = collision.gameObject.GetComponent<Dash>();
 
-        if (estadoJugador != null && estadoJugador.Estado() == estado.Dash)
+        if (estadoAct != null && (estadoAct.Estado() == estado.MovimientoGancho || estadoAct.Estado() == estado.Dash))
         {
             transform.parent.gameObject.SetActive(false);
+            dash.ReDashea();
         }
     }
 }
