496c498e10666cabf5e51fc5fa0960d85424507f
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-05-29 18:25:07 +0200

    Aun mas refinado lo del timer

--- Assets/Scripts/SistemaVidas/MuertePorCaida.cs
+++ Assets/Scripts/SistemaVidas/MuertePorCaida.cs
@@ -4,9 +4,6 @@
 
 public class MuertePorCaida : MonoBehaviour
 {
-    [SerializeField]AgitarCamara agitarCamara;
-    [SerializeField]EfectoJugadorDanyado efectoJugadorDañado;
-
     [SerializeField] AudioSource aud;
 
     void OnTriggerEnter2D(Collider2D collision) //cuando alguna entidad entre al "trigger"
@@ -16,7 +13,6 @@ public class MuertePorCaida : MonoBehaviour
         if (jugador != null && jugador.Estado() != estado.Muerte) //en caso de que lo fuera
         {
             jugador.CambioEstado(estado.Muerte); //cambiamos su estado a Muerte
-            
             aud.Play();
             GameManager.instance.Muerte();
         }
