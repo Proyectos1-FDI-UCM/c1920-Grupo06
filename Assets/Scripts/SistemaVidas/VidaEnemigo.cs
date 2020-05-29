4230a02aef16f3910625be3bbbc515be14a63196
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-05-29 18:58:53 +0200

    Arreglado zona de aparicion de las partículas (ahora bien); y, por tanto, arreglados errores de reaparicion y particulas con el Timer

--- Assets/Scripts/SistemaVidas/VidaEnemigo.cs
+++ Assets/Scripts/SistemaVidas/VidaEnemigo.cs
@@ -4,12 +4,21 @@
 
 public class VidaEnemigo : MonoBehaviour
 {
+    ParticulasMuerteEnemigos particulas = null;
+
+    private void Start()
+    {
+        particulas = GetComponent<ParticulasMuerteEnemigos>();
+    }
+
     public void EliminarEnemigo(GameObject other)
     {
         //si el jugador esta haciendo dash/gancho sobre el enemigo, destruimos al enemigo
         Estados estados = other.GetComponent<Estados>();
         if (other != null && estados.Estado() == estado.Dash)
         {
+            //Activo las partículas
+            if (particulas != null) particulas.Activar();
             other.GetComponent<Jugador>().estadisticas.Enemigo(); //Sumamos uno a enemigos derrotados
             gameObject.SetActive(false);
             GameManager.instance.ContadorEnemigosElim();
