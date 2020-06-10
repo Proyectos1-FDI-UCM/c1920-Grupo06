d3e8c46c6714cf6eda01bf699e6cdc2ad8852788
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2020-06-10 17:38:22 +0200

    Reimplementado script borrado por accidente (ActivarNivel2Menu). Corregido triggers de plataformas agrietadas

---
+++ Assets/Scripts/Menus/ActivarNivel2Menu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ActivarNivel2Menu : MonoBehaviour
+{
+    public void ActivarNivel2()
+    {
+        if (!LevelManager.instance.Nivel2()) gameObject.SetActive(false);
+        else gameObject.SetActive(true);
+    }
+}
